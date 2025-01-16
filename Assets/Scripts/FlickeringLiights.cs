e016fdeed301f093ec3719baba24f964c77072fe
keonakalaja <kalajakeona05@gmail.com>
2025-01-16 21:53:39 +0100

    Fixed the environment of Scene1

--- Assets/Scripts/FlickeringLiights.cs
+++ Assets/Scripts/FlickeringLiights.cs
@@ -1,29 +1,26 @@
 using UnityEngine;
 using System.Collections;
+
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public bool isFlickering = false;
     public float timeDelay;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Start()
     {
-        if (isFlickering == false)
-        {
-            StartCoroutine(FlickeringLights());
-        }
+        StartCoroutine(FlickeringLights());
     }
 
     IEnumerator FlickeringLights()
     {
-        isFlickering = true;
-        this.gameObject.GetComponent<Light>().enabled = false;
-        timeDelay = Random.Range(0.01f, 0.2f);
-        yield return new WaitForSeconds(timeDelay);
-        this.gameObject.GetComponent<Light>().enabled = true;
-        timeDelay = Random.Range(0.01f, 0.2f);
-        yield return new WaitForSeconds(timeDelay);
-        isFlickering = false;
+        while (true) // Infinite loop to continuously flicker the light
+        {
+            this.gameObject.GetComponent<Light>().enabled = false;
+            timeDelay = Random.Range(0.01f, 0.2f);
+            yield return new WaitForSeconds(timeDelay);
+            this.gameObject.GetComponent<Light>().enabled = true;
+            timeDelay = Random.Range(0.01f, 0.2f);
+            yield return new WaitForSeconds(timeDelay);
+        }
     }
 }
