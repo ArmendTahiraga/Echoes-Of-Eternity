99777f56b1074da87ea1b3ec99b45c793ececf35
Armend <armend_tahiraga@outlook.com>
2025-02-17 15:15:46 +0100

    Created the starting monologue of the warf scene, created a script for monologues

---
+++ Assets/Scripts/MonologueInteractable.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using DS;
+using UnityEngine;
+
+public class MonologueInteractable : MonoBehaviour {
+    [SerializeField] private DialogueUI dialogueUI;
+    [SerializeField] private PlayerMovement playerMovement;
+    [SerializeField] private PlayerCam playerCam;
+    [SerializeField] private string characterName;
+    [SerializeField] private bool isOneTime;
+    [SerializeField] private bool hasBeenPlayed;
+    [SerializeField] private float delay;
+    
+    private IEnumerator PlayMonologue() {
+        yield return new WaitForSeconds(delay);
+        Interact();
+    }
+    
+    public void Interact() {
+        playerMovement.isPlayerMoving = false;
+        playerCam.lockCamera = true;
+        dialogueUI.StartDialogue(characterName, gameObject.GetComponent<DSDialogue>());
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            if (isOneTime && !hasBeenPlayed) {
+                StartCoroutine(PlayMonologue());
+            }
+        }
+    }
+}
