5305685312db18ac1a7c3667c512d7a0deac77b1
Armend <armend_tahiraga@outlook.com>
2025-03-07 23:57:29 +0100

    Made some small adjustments to the scenes

--- Assets/Scripts/NPCInteractable.cs
+++ Assets/Scripts/NPCInteractable.cs
@@ -30,6 +30,7 @@ public class NPCInteractable : MonoBehaviour, Interactable {
         if (animationTriggers?.Length > 0) {
             for (int i = 0; i < animationTriggers.Length; i++) {
                 if (animationTriggers[i] == "PlayerSitTrigger") {
+                    GameObject.Find("Player").GetComponent<PlayerMovement>().isPlayerMoving = false;
                     teleportPlayer.MovePlayer(new Vector3(-3.632f, 45.9f, 190.45f), Quaternion.Euler(0, 90, 0) , new Vector3(0, -1.971f, 0.019f),
                         new Vector3(0, -0.738f, -0.683f), Quaternion.Euler(0, -265f, 0), Quaternion.Euler(0, 0, 0));
                 }
