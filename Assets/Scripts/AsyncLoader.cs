e3cb7d6f8a1430b45b36dd10012ade87d9729b30
Armend <armend_tahiraga@outlook.com>
2025-03-09 23:40:53 +0100

    Added a fade animation when a new scene is loaded

--- Assets/Scripts/AsyncLoader.cs
+++ Assets/Scripts/AsyncLoader.cs
@@ -7,18 +7,25 @@ public class AsyncLoader : MonoBehaviour {
     [SerializeField] private GameObject loadingScreen;
     [SerializeField] private GameObject gameUI;
     [SerializeField] private Slider loadingSlider;
+    [SerializeField] private Animator transitionAnimator;
 
     public void LoadLevel(string levelToLoad) {
         gameUI.SetActive(false);
+        transitionAnimator.SetTrigger("FadeIn");
+        StartCoroutine(StartLoading(levelToLoad));
+    }
+
+    private IEnumerator StartLoading(string levelToLoad) {
+        yield return new WaitForSeconds(1f);
         loadingScreen.SetActive(true);
         StartCoroutine(LoadLevelAsync(levelToLoad));
     }
 
-    IEnumerator LoadLevelAsync(string levelToLoad) {
+    private IEnumerator LoadLevelAsync(string levelToLoad) {
         AsyncOperation loadOperation = SceneManager.LoadSceneAsync(levelToLoad);
 
         while (!loadOperation.isDone) {
-            float progress = Mathf.Clamp01(loadOperation.progress / 0.9f);
+            float progress = Mathf.Clamp01(loadOperation.progress / 0.99f);
             loadingSlider.value = progress;
             yield return null;
         }
